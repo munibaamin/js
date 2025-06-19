d8b275a302737cdd30ccd9cd0ae9b6f8d32fd706
munibaamin <munibaamindev@gmail.com>
2025-06-19 06:49:11 +0000

    variables

---
+++ 02-data-types.js
@@ -0,0 +1,10 @@
+
+// "use strict";
+let name = "Hetesh"
+accountEmail = "muniba@.gmail.com"
+console.log(accountEmail )
+const accounId = 144553
+console.log(name)
+name = "muniba"
+console.log(name)
+
