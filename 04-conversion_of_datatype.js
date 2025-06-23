cc2973e989f185a251f5a6928d985cd5bf508113
munibaamin <munibaamindev@gmail.com>
2025-06-23 06:34:55 +0000

    coversion

---
+++ 04-conversion_of_datatype.js
@@ -0,0 +1,4 @@
+let score = 33
+let realScore = String(score)
+console.log(realScore)
+console.log(typeof realScore)
---
+++ conversion-operation.js
@@ -0,0 +1,8 @@
+// 
+// console.log(true)
+// let num1, num2, num3 = 2 + 2
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
